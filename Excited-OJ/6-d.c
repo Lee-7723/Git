05863b6066846e2dbed7c02b738d81d327213685
Lee <1981671849@qq.com>
2019-11-24 12:42:19 +0800

    2019-11-24

--- Excited-OJ/6-d.c
+++ Excited-OJ/6-d.c
@@ -1,17 +1,35 @@
 #include<stdio.h>
 int main()
 {
-	float a, b, c;
-	int A, B, C,D,E;
-	scanf("%f %f %f", &a, &b, &c);
-	A = 1000 * a;
-	B = 1000 * b;
-	C = 1000 * c;
-	if (A >= B) D = A - B;
-	else D = B - A;
-	if (B >= C) E = B - C;
-	else E = C - B;
-	if (E == D) printf("yes");
-	else printf("no");
-	return 0;
+	char tst[10][50];//用于测试的字符
+	int grp,cnt[10] = { 0 }, ii = 0,a=1;//grp组数，cnt每行字符个数
+	scanf("%d", &grp);
+	for (int i = 0; i < grp; i++)
+	{
+		scanf("%s", &tst[i]);
+		while (tst[i][cnt[i]] != '\0')
+		{
+			cnt[i]++;//用于计算每一行有多少个字符
+		}
+		ii = 0;
+	}
+	for (int i = 0; i < grp; i++)
+	{
+		if (cnt[i] > 31) printf("no");//判断字符个数是否超过31
+		else if (tst[i][0] == '_' || (tst[i][0]>='A'&& tst[i][0] <= 'Z')|| (tst[i][0] >= 'a' && tst[i][0] <= 'z'))//判断第一个字符是否是下划线或字母
+		{
+			for (int ii = 1; ii < cnt[i]; ii++)
+			{
+				if (tst[i][ii] == '_' || (tst[i][ii] >= '0' && tst[i][ii] <= '9') || (tst[i][ii] >= 'A' && tst[i][ii] <= 'Z') || (tst[i][ii] >= 'a' && tst[i][ii] <= 'z'))a++;//判断其他字符，a用来数合法字符的个数
+				else break;
+			}
+			if (a == cnt[i])printf("yes\n");//如果合法字符个数和全部字符个数一样多就合法
+			else printf("no\n");
+			a = 1;
+		}
+		else
+		{
+			printf("no\n");
+		}
+	}
 }
